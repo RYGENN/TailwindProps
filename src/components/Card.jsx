c78ce283ce0f31d29e0fb6bb55c834b623e0d7d4
Anupam Mahato <91363907+RYGENN@users.noreply.github.com>
2023-12-01 00:36:27 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Card(props) {  // otherwise we can use direct {username} instead of props
+function Card(props) { // otherwise we can use direct {username} instead of props
     return(
 <div class="relative h-[400px] w-[300px] rounded-md">
   <img
