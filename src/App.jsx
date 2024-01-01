6718e2ab1c7743b33608054e2446a6717e826cb7
anupam <anupammahato357@gmail.com>
2024-01-01 22:32:07 +0530

    2nd

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Card from './components/card'
+import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0)  
+
+  let about = {
+    Hela_passion:"I like to play football and watch cricket",
+    kesy_aim:"To be a millionire"
+  }
   
   return (
     <>
       <h1 className='bg-yellow-400 rounded-xl p-2 text-black mb-4'>Tailwind CSS</h1>
-      <Card username='Angelina' />
-      <Card username='Hela'/>
-      <Card username='kesy'/>
+      <Card username='Angelina' hero={about.Hela_passion} /> 
+      <Card username='Hela' />
+      <Card username='kesy' hero={about.kesy_aim}/> 
     </>
   )
 }
 
-export default App
+export default App;
+
